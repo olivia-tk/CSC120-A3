fcf85cc7626716667ab0c979756f471bfad6e420
Olivia Kraft <okraft@smith.edu>
2024-02-08 20:50:22 -0500

    Finished assignment

--- Conversation.java
+++ Conversation.java
@@ -4,3 +4,20 @@ class Conversation {
     // You will start the conversation here.
   }
 }
+
+import java.util.Scanner
+public class ChatbotLite {
+  piblic static void main(String[])
+  Scanner sc = new Scanner(System.in);
+
+  while (stillGoing) {
+
+    //Get user's statement/input 
+    sc.nextLine():
+    //Respond
+    if (userInput.co)
+    System.out.println(x:"How interesting...");
+
+  }
+
+}
